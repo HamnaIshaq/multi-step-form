3f0907cdfb84449d20d3913bce8a6edb3001b70a
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 23:24:19 +0500

    Show Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
+import Card from './components/Card';
 
 function App() {
   return (
     <div>
       <Sidebar/>
+      <Card/>
     </div>
   )
 }
