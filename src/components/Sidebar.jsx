46cca369fb343bfb7fd9285bdc9e9f9c671d9693
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 19:23:11 +0500

    First commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import '../assets/styles/styles.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container">
+      <ul className="sidebar-list">
+        <li className="sidebar-list-item">
+          <button type="button" className="btn flex">
+            <span className="step">1</span>
+            <span className="sidebar-item-name flex flex-vertical align-start">
+              <span className="step-num">Step 1</span>
+              <strong className="">your info</strong>
+            </span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
