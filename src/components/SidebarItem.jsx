563a0a64fc705873efba1663a16f2f6a5f107a45
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 20:51:57 +0500

    Make a reusable sidebar list item

---
+++ src/components/SidebarItem.jsx
@@ -0,0 +1,15 @@
+const SidebarItem = ({ number, stepNumber, heading }) => {
+  return (
+    <li className="sidebar-list-item">
+      <button type="button" className="btn flex">
+        <span className="step">{number}</span>
+        <span className="sidebar-item-name flex flex-vertical align-start hide">
+          <span className="step-num">{stepNumber}</span>
+          <strong className="">{heading}</strong>
+        </span>
+      </button>
+    </li>
+  );
+}
+
+export default SidebarItem;
