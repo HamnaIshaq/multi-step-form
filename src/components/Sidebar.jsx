822913e11e2f8bd479dd9ae0f175a1fde699d854
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 20:52:28 +0500

    Add SidebarItem component to show list item

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,14 @@
 import '../assets/styles/styles.css';
+import SidebarItem from './SidebarItem'; 
 
 const Sidebar = () => {
   return (
     <div className="sidebar-container">
       <ul className="sidebar-list flex justify-center">
-        <li className="sidebar-list-item">
-          <button type="button" className="btn flex">
-            <span className="step">1</span>
-            <span className="sidebar-item-name flex flex-vertical align-start hide">
-              <span className="step-num">Step 1</span>
-              <strong className="">your info</strong>
-            </span>
-          </button>
-        </li>
+        <SidebarItem number="1" stepNumber="Step 1" heading="your info"/>
+        <SidebarItem number="2" stepNumber="Step 2" heading="select plan"/>
+        <SidebarItem number="3" stepNumber="Step 3" heading="add-ons"/>
+        <SidebarItem number="4" stepNumber="Step 4" heading="summary"/>
       </ul>
     </div>
   );
