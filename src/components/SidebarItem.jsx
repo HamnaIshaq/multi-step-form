c0f29a3a69d169d494d1f011b15ceca745375adc
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 23:22:30 +0500

    Show sidebar item

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -1,8 +1,8 @@
-const SidebarItem = ({ number, stepNumber, heading, active }) => {
+const SidebarItem = ({ number, stepNumber, heading }) => {  
   return (
     <li className="sidebar-list-item">
       <button type="button" className="btn flex">
-        <span className={`step ${active}`}>{number}</span>
+        <span className="step">{number}</span>
         <span className="sidebar-item-name flex flex-vertical align-start hide">
           <span className="step-num">{stepNumber}</span>
           <strong className="">{heading}</strong>
