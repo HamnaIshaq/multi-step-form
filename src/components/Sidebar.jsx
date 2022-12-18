f1dacfe2af30d54b35a0a42f6c4f0cb790f7a592
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 23:23:31 +0500

    Add sidebarItems array and loop through them to show sidebar items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,44 @@
 import '../assets/styles/styles.css';
 import SidebarItem from './SidebarItem'; 
 
+const sideBarItems = [
+  {
+    number: "1",
+    stepNumber: "step 1",
+    heading: "your info"
+  },
+  {
+    number: "2",
+    stepNumber: "step 2",
+    heading: "select plan"
+  },
+  {
+    number: "3",
+    stepNumber: "step 3",
+    heading: "add-ons"
+  },
+  {
+    number: "4",
+    stepNumber: "step 4",
+    heading: "summary"
+  }
+]
+
 const Sidebar = () => {
+
   return (
     <div className="sidebar-container">
       <ul className="sidebar-list flex justify-center">
-        <SidebarItem number="1" stepNumber="Step 1" heading="your info" active="active"/>
-        <SidebarItem number="2" stepNumber="Step 2" heading="select plan" active=""/>
-        <SidebarItem number="3" stepNumber="Step 3" heading="add-ons" active=""/>
-        <SidebarItem number="4" stepNumber="Step 4" heading="summary" active=""/>
+        { sideBarItems.map(item => {
+          return (
+            <SidebarItem 
+              key={item.number} 
+              number={item.number} 
+              stepNumber={item.stepNumber} 
+              heading={item.heading}
+            />
+          )
+        })}
       </ul>
     </div>
   );
