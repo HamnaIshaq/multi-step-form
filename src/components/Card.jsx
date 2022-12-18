d4d2e6f28d32657b9ea9f43de374cba0c23859bc
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 23:23:52 +0500

    Add Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div className="card-container">
+      <h2 className="card-h2">Personal info</h2>
+      <p className="card-description">Please provide your name, email address, and phone number</p>
+    </div>
+  )
+}
+
+export default Card;
