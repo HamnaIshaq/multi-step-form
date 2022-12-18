43993a35071e2cd235396f6babeaeb676cf848aa
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-18 20:32:54 +0500

    Center sidebar items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,11 +3,11 @@ import '../assets/styles/styles.css';
 const Sidebar = () => {
   return (
     <div className="sidebar-container">
-      <ul className="sidebar-list">
+      <ul className="sidebar-list flex justify-center">
         <li className="sidebar-list-item">
           <button type="button" className="btn flex">
             <span className="step">1</span>
-            <span className="sidebar-item-name flex flex-vertical align-start">
+            <span className="sidebar-item-name flex flex-vertical align-start hide">
               <span className="step-num">Step 1</span>
               <strong className="">your info</strong>
             </span>
