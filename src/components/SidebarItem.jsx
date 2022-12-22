707228c5c7ed1be1f04d394e389ad77bcb463231
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-22 23:19:58 +0500

    Replace button tag with li tag

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -1,13 +1,13 @@
 const SidebarItem = ({ number, stepNumber, heading }) => {  
   return (
     <li className="sidebar-list-item">
-      <button type="button" className="btn flex">
+      <div className="btn flex">
         <span className="step">{number}</span>
         <span className="sidebar-item-name flex flex-vertical align-start hide">
           <span className="step-num">{stepNumber}</span>
           <strong className="">{heading}</strong>
         </span>
-      </button>
+      </div>
     </li>
   );
 }
