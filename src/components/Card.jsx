2e513fe06245d6af8f4c464e6b16ac8e63ee559d
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-22 23:27:00 +0500

    Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
-const Card = () => {
+const Card = ({ heading, description, thankYou, children }) => {
+  const thankYouImg = thankYou && (
+      <img className="thank-you-img" src={thankYou} alt="checkmark"/>
+    )
+
   return (
     <div className="card-container">
-      <h2 className="card-h2">Personal info</h2>
-      <p className="card-description">Please provide your name, email address, and phone number</p>
+      {thankYouImg}
+      <h2 className="card-h2">{heading}</h2>
+      <p className="card-description">{description}</p>
+      {children}  
     </div>
   )
 }
