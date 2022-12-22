c6e60e0f533e0fe98af711230cdf1cbed98bd6e0
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-22 23:16:31 +0500

    Show pages using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SharedLayout from './pages/SharedLayout';
+import PersonalInfo from './pages/PersonalInfo';
+import SelectPlan from './pages/SelectPlan';
+import AddOns from './pages/AddOns';
+import Summary from './pages/Summary';
+import ThankYou from './pages/ThankYou';
+import Error from './pages/Error';
 import Sidebar from './components/Sidebar';
-import Card from './components/Card';
+import FormFooter from './components/FormFooter';
 
-function App() {
+
+const App = () => {
+
   return (
-    <div>
-      <Sidebar/>
-      <Card/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout/>}>
+          <Route index element={<PersonalInfo/>}/>
+          <Route path="/selectplan" element={<SelectPlan/>}/>
+          <Route path="/addons" element={<AddOns/>}/>
+          <Route path="/summary" element={<Summary/>}/>
+          <Route path="/thankyou" element={<ThankYou/>}/>
+        </Route>
+        <Route path="*" element={<Error/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
