1790e2df290dafae0a56b5e9447c85f5c7b10c3a
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-23 23:09:32 +0500

    Add selected user plan data inside user plan state

--- src/components/RadioCard.jsx
+++ src/components/RadioCard.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
 import ArcadeIcon from '../assets/images/icon-arcade.svg';
 import AdvancedIcon from '../assets/images/icon-advanced.svg';
 import ProIcon from '../assets/images/icon-pro.svg';
 
-const RadioCard = () => {
-  const [userplan, setUserPlan] = useState("");
+const RadioCard = ({ userPlan, setUserPlan }) => {
 
-  const onPlanSelection = (e) => setUserPlan(e.target.value);
+  const onPlanSelection = (e) => {
+    setUserPlan({ plan: e.target.value, planCost: parseInt(e.target.getAttribute('data-plan-cost')) });
+  }
 
   return (
     <fieldset className="radio-input-container">
       <div>
-        <input type="radio" id="plan-arcade" name="plan" className="radio-input" value="arcade" onClick={onPlanSelection}/>
+        <input type="radio" id="plan-arcade" name="plan" className="radio-input" value="arcade" data-plan-cost="9" onClick={onPlanSelection}/>
         <label htmlFor="plan-arcade" className="input-field selection-input-card">
           <img className="plan-img" src={ArcadeIcon} alt="game" />
           <div className="plan-details">
@@ -23,7 +23,7 @@ const RadioCard = () => {
       </div>
 
       <div>
-        <input type="radio" id="plan-advanced" name="plan" className="radio-input" value="advanced" onClick={onPlanSelection}/>
+        <input type="radio" id="plan-advanced" name="plan" className="radio-input" value="advanced" data-plan-cost="12" onClick={onPlanSelection}/>
         <label htmlFor="plan-advanced" className="input-field selection-input-card">
         <img className="plan-img" src={AdvancedIcon} alt="game" />
         <div className="plan-details">
@@ -35,7 +35,7 @@ const RadioCard = () => {
       </div>
 
       <div>
-        <input type="radio" id="plan-pro" name="plan" className="radio-input" value="pro" onClick={onPlanSelection}/>
+        <input type="radio" id="plan-pro" name="plan" className="radio-input" value="pro" data-plan-cost="15" onClick={onPlanSelection}/>
           <label htmlFor="plan-pro" className="input-field selection-input-card">
           <img className="plan-img" src={ProIcon} alt="game" />
           <div className="plan-details">
