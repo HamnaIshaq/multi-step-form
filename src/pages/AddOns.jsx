f21cc179f9a6f8f528023609c56d415e70a7cb9c
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-23 23:10:38 +0500

    Pass add ons state to CheckboxCard component

--- src/pages/AddOns.jsx
+++ src/pages/AddOns.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 import CheckboxCard from '../components/CheckboxCard';
 
-const AddOns = () => {
+const AddOns = ({ userAddOns, setUserAddOns }) => {
   return (
     <div>
       <Card heading="Pick add-ons" description="Add-ons help enhance your gaming experience.">
-        <CheckboxCard/>
+        <CheckboxCard userAddOns={userAddOns} setUserAddOns={setUserAddOns}/>
       </Card>
       <div className="form-footer-container flex justify-space-between">
         <Link to="/selectplan">
