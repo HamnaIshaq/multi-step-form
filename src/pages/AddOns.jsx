8d38e759f516dd70585726b44d95466e8025bc26
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-26 23:36:34 +0500

    Add add ons  monthly/yearly data

--- src/pages/AddOns.jsx
+++ src/pages/AddOns.jsx
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 import CheckboxCard from '../components/CheckboxCard';
 
-const AddOns = ({ userAddOns, setUserAddOns }) => {
+const AddOns = ({ userAddOns, setUserAddOns, planTypeYearly }) => {
+  const addOnsMonthly = { "Online service": 1, "Larger storage": 2, "Customizable profile": 2 };
+  const addOnsYearly = { "Online service": 10, "Larger storage": 20, "Customizable profile": 20 };
+
   return (
     <div>
       <Card heading="Pick add-ons" description="Add-ons help enhance your gaming experience.">
-        <CheckboxCard userAddOns={userAddOns} setUserAddOns={setUserAddOns}/>
+        <CheckboxCard 
+          userAddOns={userAddOns} 
+          setUserAddOns={setUserAddOns} 
+          addOnsMonthly={addOnsMonthly} 
+          addOnsYearly={addOnsYearly} 
+          planTypeYearly={planTypeYearly}
+        />
       </Card>
       <div className="form-footer-container flex justify-space-between">
         <Link to="/selectplan">
