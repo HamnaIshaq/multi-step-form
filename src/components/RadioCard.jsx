abe4aa7dca9572829068556d228f70a0d7c7b78d
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-26 23:38:03 +0500

    Show month/year data on selected plan type

--- src/components/RadioCard.jsx
+++ src/components/RadioCard.jsx
@@ -2,12 +2,12 @@ import ArcadeIcon from '../assets/images/icon-arcade.svg';
 import AdvancedIcon from '../assets/images/icon-advanced.svg';
 import ProIcon from '../assets/images/icon-pro.svg';
 
-const RadioCard = ({ userPlan, setUserPlan }) => {
+const RadioCard = ({ userPlan, setUserPlan, planTypeYearly, planCostMonthly, planCostYearly }) => {
 
   const onPlanSelection = (e) => {
     setUserPlan({ plan: e.target.value, planCost: parseInt(e.target.getAttribute('data-plan-cost')) });
   }
-
+  
   return (
     <>
       <div>
@@ -17,16 +17,19 @@ const RadioCard = ({ userPlan, setUserPlan }) => {
           name="plan" 
           className="radio-input" 
           value="arcade" 
-          data-plan-cost="9" 
-          checked={userPlan.plan === 'arcade'}
+          data-plan-cost={planTypeYearly ? planCostYearly.arcade : planCostMonthly.arcade } 
+          checked={userPlan.plan === "arcade"}
           onChange={onPlanSelection}
         />
         <label htmlFor="plan-arcade" className="input-field selection-input-card">
           <img className="plan-img" src={ArcadeIcon} alt="game" />
           <div className="plan-details">
             <span className="plan-name">Arcade</span>
-            <span className="plan-cost">$9/<abbr className="plan-duration" title="month">mo</abbr></span>
-            <span className="input-label plan-free-duration hide">2 months free</span>
+            <span className="plan-cost">${planTypeYearly ? planCostYearly.arcade : planCostMonthly.arcade}/
+              <abbr className="plan-duration" title={planTypeYearly ? "year" : "month" }>
+                {planTypeYearly ? "yr" : "mo" }
+              </abbr></span>
+            <span className={`input-label plan-free-duration ${planTypeYearly ? "" : "hide"}`}>2 months free</span>
           </div>
         </label>
       </div>
@@ -38,16 +41,16 @@ const RadioCard = ({ userPlan, setUserPlan }) => {
           name="plan" 
           className="radio-input" 
           value="advanced" 
-          data-plan-cost="12" 
-          checked={userPlan.plan === 'advanced'}
+          data-plan-cost={planTypeYearly ? planCostYearly.advanced : planCostMonthly.advanced } 
+          checked={userPlan.plan === "advanced"}
           onChange={onPlanSelection}
         />
         <label htmlFor="plan-advanced" className="input-field selection-input-card">
         <img className="plan-img" src={AdvancedIcon} alt="game" />
         <div className="plan-details">
           <span className="plan-name">Advanced</span>
-          <span className="plan-cost">$12/<abbr className="plan-duration" title="month">mo</abbr></span>
-          <span className="input-label plan-free-duration hide">2 months free</span>
+          <span className="plan-cost">${planTypeYearly ? planCostYearly.advanced : planCostMonthly.advanced}/<abbr className="plan-duration" title={planTypeYearly ? "year" : "month" }>{planTypeYearly ? "yr" : "mo" }</abbr></span>
+          <span className={`input-label plan-free-duration ${planTypeYearly ? "" : "hide"}`}>2 months free</span>
         </div>
       </label>
       </div>
@@ -59,15 +62,15 @@ const RadioCard = ({ userPlan, setUserPlan }) => {
           name="plan" 
           className="radio-input" 
           value="pro" 
-          data-plan-cost="15"
-          checked={userPlan.plan === 'pro'}
+          data-plan-cost={planTypeYearly ? planCostYearly.pro : planCostMonthly.pro } 
+          checked={userPlan.plan === "pro"}
           onChange={onPlanSelection}/>
           <label htmlFor="plan-pro" className="input-field selection-input-card">
           <img className="plan-img" src={ProIcon} alt="game" />
           <div className="plan-details">
             <span className="plan-name">Pro</span>
-            <span className="plan-cost">$15/<abbr className="plan-duration" title="month">mo</abbr></span>
-            <span className="input-label plan-free-duration hide">2 months free</span>
+            <span className="plan-cost">${planTypeYearly ? planCostYearly.pro : planCostMonthly.pro}/<abbr className="plan-duration" title={planTypeYearly ? "year" : "month" }>{planTypeYearly ? "yr" : "mo" }</abbr></span>
+            <span className={`input-label plan-free-duration ${planTypeYearly ? "" : "hide"}`}>2 months free</span>
           </div>
         </label>
       </div>
