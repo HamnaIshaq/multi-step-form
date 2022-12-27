116bf159737161f192810cdf44a003062535c8b4
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-27 22:19:07 +0500

    Add class "sidebar-item-container for desktop"

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -1,9 +1,9 @@
 const SidebarItem = ({ number, stepNumber, heading }) => {  
   return (
     <li className="sidebar-list-item">
-      <div className="btn flex">
+      <div className="btn flex sidebar-item-container">
         <span className="step">{number}</span>
-        <span className="sidebar-item-name flex flex-vertical align-start hide">
+        <span className="sidebar-item-name flex flex-vertical align-start">
           <span className="step-num">{stepNumber}</span>
           <strong className="">{heading}</strong>
         </span>
