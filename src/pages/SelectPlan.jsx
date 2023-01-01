3cd7a4be986928c3325c63572eddfcf31ed031f2
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-01 21:10:29 +0500

    Pass monthly/yearly plan cost data as props

--- src/pages/SelectPlan.jsx
+++ src/pages/SelectPlan.jsx
@@ -3,10 +3,7 @@ import Card from '../components/Card';
 import RadioCard from '../components/RadioCard';
 import PlanType from '../components/PlanType';
 
-const SelectPlan = ({ userPlan, setUserPlan, planTypeYearly, setPlanTypeYearly, userAddOns, setUserAddOns }) => {
-  
-  const planCostMonthly = { arcade: 9, advanced: 12, pro: 15 };
-  const planCostYearly = { arcade: 90, advanced: 120, pro: 150 };
+const SelectPlan = ({ planCostMonthly, planCostYearly, userPlan, setUserPlan, planTypeYearly, setPlanTypeYearly, userAddOns, setUserAddOns }) => {
 
   return (
     <div className="form-container">
